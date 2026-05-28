2c9f34ddf864975f2d894257a3431b850e8ce081
boburov <boburovdev@gmail.com>
2026-05-28 17:37:53 +0500

    fix(config): remove outputFileTracingRoot and turbopack settings from next.config.ts files

--- courier/next.config.ts
+++ courier/next.config.ts
@@ -2,10 +2,7 @@ import type { NextConfig } from "next";
 import path from "node:path";
 
 const nextConfig: NextConfig = {
-  outputFileTracingRoot: path.join(__dirname),
-  turbopack: {
-    root: path.join(__dirname),
-  },
+
 };
 
 export default nextConfig;
